b882d8b005f553ef5914f6bd8d32132d6b5ab22e
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-06-29 02:47:52 -0700

    save stake

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,12 +7,12 @@ import { Stake, User } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handleStakeLocked(event: StakeLocked): void {
-  let stake = Stake.load(event.params.kek_id.toString());
+  let stake = Stake.load(event.params.kek_id.toHexString());
   if (!stake) {
     let stakeId = event.params.kek_id;
     let startTimestamp = event.block.timestamp;
     let lockDuration = event.params.secs;
-    stake = new Stake(stakeId.toString());
+    stake = new Stake(stakeId.toHexString());
     stake.amountLocked = event.params.amount;
     stake.startTimestamp = startTimestamp.toI32();
     stake.lockDuration = lockDuration.toI32();
@@ -20,7 +20,7 @@ export function handleStakeLocked(event: StakeLocked): void {
   }
   let userParam = event.params.user.toHexString();
   stake.owner = userParam;
-
+  stake.save();
   let user = User.load(userParam);
   if (!user) {
     user = new User(userParam);
@@ -29,10 +29,11 @@ export function handleStakeLocked(event: StakeLocked): void {
 }
 
 export function handleStakeWithdrawn(event: WithdrawLocked): void {
-  let stake = Stake.load(event.params.kek_id.toString());
+  let stake = Stake.load(event.params.kek_id.toHexString());
   if (!stake) {
-    stake = new Stake(event.params.kek_id.toString());
+    stake = new Stake(event.params.kek_id.toHexString());
   }
   stake.withdrawTimestamp = event.block.timestamp.toI32();
   stake.amountLocked = new BigInt(0);
+  stake.save();
 }
